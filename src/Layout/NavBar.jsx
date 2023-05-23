914c4941530f907b0a8ec53dec9f556af8361364
unknown <jacksonanderson1515@hotmail.com>
2023-05-23 17:39:15 -0300

    navbar + css

--- src/Layout/NavBar.jsx
+++ src/Layout/NavBar.jsx
@@ -1,5 +1,28 @@
+import { Link } from 'react-router-dom'
+import  './NavBar.css'
+
 function NavBar(){
-    return <h1>NavBar!</h1>
+    return (
+    <nav className="navbar">
+        <ul className="list">
+            <li className="item">
+                <Link to="/">HOME</Link>
+            </li>
+            <li className="item">
+                <Link to="/login">LOGIN</Link>
+            </li>
+            <li className="item">
+                <Link to="/clients">CLIENTES</Link>
+            </li>
+            <li className="item">
+                <Link to="/books">LIVROS</Link>
+            </li>
+            <li className="item">
+                <Link to="/purchase">COMPRAS</Link>
+            </li>
+        </ul>
+    </nav>
+    )
 }
 
 export default NavBar;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import NavBar from './Layout/NavBar';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <NavBar />
   </React.StrictMode>
+  </BrowserRouter>
 );
 reportWebVitals();
