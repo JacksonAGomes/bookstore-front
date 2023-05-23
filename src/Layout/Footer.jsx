2c206dd89469aa0eec18aaaa557f4711d132370f
Irla Ingrid <irla.ingrid@esig.com.br>
2023-05-23 18:00:52 -0300

    footer

---
+++ src/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer(){
+    return(
+        <footer className='footer'>
+            Desenvolvido por Jackson, Marta e Irla
+        </footer>
+        )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import NavBar from './Layout/NavBar';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import Footer from './Layout/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <React.StrictMode>
     <NavBar />
+    <Footer />
   </React.StrictMode>
   </BrowserRouter>
 );
