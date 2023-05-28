f2b00cab71399d87d30c04c62a28adc5e56b4138
unknown <jacksonanderson1515@hotmail.com>
2023-05-28 20:04:44 -0300

    adicionando listagem de clientes

--- src/Components/clients/Client.jsx
+++ src/Components/clients/Client.jsx
@@ -1,22 +1,54 @@
-import { useState } from "react";
 import './Client.css'
 import { Navigate } from "react-router-dom"
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Client() {
   const [clicked, setClicked] = useState(false);
+  const [clients, setClients] = useState(null);
 
-    return (
-            <div className="buttons">
-                <button onClick={()=>{
-                    setClicked(true);
-                }} id="bSave">Novo Cliente</button>
+  useEffect(() => {
+    axios.get('http://localhost:8080/clients')
+      .then(response => setClients(response.data))
+      .catch(error => console.log(error));
+  }, []);
 
-                {
-                  clicked &&  <Navigate replace to="/clients/new"/>     
-                }
-          </div>
+  return (
+    <div className='clientList'>
+    <div className="buttons">
+      <button onClick={() => {
+        setClicked(true);
+      }} id="bSave">Novo Cliente</button>
 
-          
-    )
+      <div className='tabela'>
+          <table>
+            <thead>
+              <tr className='itemList'>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Idade</th>
+                <th>Gênero</th>
+                <th>Telefone</th>
+              </tr>
+            </thead>
+            <tbody>
+              { clients && clients.map(client => (
+                <tr className='itemList' key={client.id}>
+                  <td>{client.name}</td>
+                  <td>{client.email}</td>
+                  <td>{client.age}</td>
+                  <td>{client.gender}</td>
+                  <td>{client.telephone}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+      </div>
+
+      {clicked && <Navigate replace to="/clients/new"/>}
+    </div>
+    </div>
+  )
 }
+
 export default Client;
