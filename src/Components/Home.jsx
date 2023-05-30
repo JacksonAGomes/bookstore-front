98703576e355963e412a2865930413bb6c731a90
unknown <jacksonanderson1515@hotmail.com>
2023-05-30 16:21:55 -0300

    alterando home e css

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,22 +1,25 @@
+import React from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom'
 
-function Home(){
-    return  (
-        <div className="home">
-        <h1>HOME</h1>
-        <h1>HOME</h1>
-        <h1>HOME</h1>
-        <h1>HOME</h1>
-        <h1>HOME</h1>
-        <h1>HOME</h1>
-        <h1>HOME</h1>
-        <h1>HOME</h1>
-        <h1>HOME</h1>
-        <h1>HOME</h1>
-        <h1>HOME</h1>
-        </div>
-    )
+function Home() {
+  const messages = [
+    "Bem-vindo à nossa livraria online!",
+    "Descubra uma vasta seleção de livros em nosso site!",
+    "Aqui você encontrará os melhores livros para leitura!",
+    "Explore nosso catálogo e encontre seu próximo livro favorito!",
+  ];
 
+  const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+
+  return (
+    <div className="home">
+      <h1 className="fixMessage">Seja bem-vindo ao Bookstore-IJM!</h1>
+      <h1>{randomMessage}</h1>
+
+      <h1>Faça agora o seu <Link className='linkStyle' to="/login">LOGIN</Link> e aproveite!</h1>
+    </div>
+  );
 }
 
 export default Home;
--- src/Layout/Top.jsx
+++ src/Layout/Top.jsx
@@ -1,25 +1,14 @@
-import { Link } from 'react-router-dom'
 import  './Top.css'
-import { useContext } from 'react';
-import { LoginContexto } from '../Context/LoginContexto';
 
 function Top(){
-    const {user} = useContext(LoginContexto);
-
     return (
         <div className='top'>
             <div className='logo'>
                 <img src='/logo2.svg' alt='logo1 icon'/>
             </div>
-
             <div>
-            {
-            user.id !== "" && 
-                <span className='message'>Olá, {user.nome}</span>
-            }
             </div>
         </div>
-
     )
 }
 
