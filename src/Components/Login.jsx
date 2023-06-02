90af94b548b456ae2c993766c26fd30a7a45247e
unknown <jacksonanderson1515@hotmail.com>
2023-06-02 20:54:06 -0300

    login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,38 +1,71 @@
 import { useState } from 'react';
-import { Navigate } from "react-router-dom"
+import { Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { LoginContexto } from '../Context/LoginContexto';
-import './Login.css'
-
-
+import './Login.css';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Login() {
-    const [clicked, setClicked] = useState (false);
-    const {setUser} = useContext(LoginContexto);
-    var userLogged = {email: "jacksonanderson@gmail.com", nome:"Jackson Anderson", id:"1"}
+  const [clicked, setClicked] = useState(false);
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const { setUser } = useContext(LoginContexto);
+  var userLogged = { email: 'jacksonanderson@gmail.com', nome: 'Jackson Anderson', id: '1' };
 
-    return (
-        <div className='login'>
-             <h1 className='text1'>Login</h1>
+  const handleLogin = () => {
+    if (email === 'irla@gmail.com' && senha === '123') {
+      setUser(userLogged);
+      toast.success('Login realizado com sucesso!', {
+        position: toast.POSITION.BOTTOM_RIGHT
+      });
+      setTimeout(() => {
+        setClicked(true);
+      }, 2000);
+    } else {
+      toast.error('Email ou senha incorretos. Por favor, tente novamente.', {
+        position: toast.POSITION.BOTTOM_RIGHT
+      });
+      setSenha('');
+      setEmail('');
+    }
+  };
 
-                <p className='text2'>Digite seu email</p>
-                <input type="email" placeholder="Email:" name="" id="1" className='input'/>
+  return (
+    <div className="login">
+      <h1 className="text1">Login</h1>
 
-                <p className='text3'>Digite sua senha</p>
-                <input type="password" placeholder="Senha:" name="" id="2" className='input'/>
+      <p className="text2">Digite seu email</p>
+      <input
+        type="email"
+        placeholder="Email:"
+        name=""
+        id="1"
+        className="input"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
-                <button onClick={()=>{ 
-                    setClicked(true);
-                    setUser(userLogged);
-                }} className='button'>
-                    Logar
-                </button>
-                {
-                clicked && 
-                    <Navigate replace to="/purchases"/>            
-                }
-        </div>
-    )      
-  }
-  
-  export default Login;
+      <p className="text3">Digite sua senha</p>
+      <input
+        type="password"
+        placeholder="Senha:"
+        name=""
+        id="2"
+        className="input"
+        value={senha}
+        onChange={(e) => setSenha(e.target.value)}
+      />
+
+      <button onClick={handleLogin} className="button">
+        Logar
+      </button>
+
+      {clicked && <Navigate replace to="/purchases" />}
+
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default Login;
